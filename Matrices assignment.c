f882707da2cf400f618d8e1bc93fed68e7e517a8
PriyangshuNaskarECE <149655415+PriyangshuNaskarECE@users.noreply.github.com>
2023-12-14 14:44:39 +0530

    Update Matrices assignment.c
    Edited: 14-12-23

--- Matrices assignment.c
+++ Matrices assignment.c
@@ -1,95 +1,84 @@
 #include <stdio.h>
+
 int main(void) {
-int a[3][3],b[3][3],c[3][3],sum[3][3],i,j,k;
-float I[3][3];
-printf("\nEnter elements of 1st matrix:\n");
-  for (int i = 0; i < 3; ++i)
-    {
-      for (int j = 0; j < 3; ++j) 
-      {
-      printf("Enter element a%d%d: ", i + 1, j + 1);
-      scanf("%d", &a[i][j]);
-      }
-    }
-   printf("Enter elements of 2nd matrix:\n");
-  for (int i = 0; i < 3; ++i)
-    {
-      for (int j = 0; j < 3; ++j) 
-      {
-      printf("Enter element b%d%d: ", i + 1, j + 1);
-      scanf("%d", &b[i][j]);
-    
-    }
-  }
+    int a[3][3], b[3][3], c[3][3], sum[3][3], i, j;
+    float I[3][3], determinant;
 
-//printing matrices
-printf("First matrix: \n");
-for(int i=0;i<3;i++){
-for(int j=0;j<3;j++){
-printf(" %d ",a[i][j]);
-}
-printf("\n");
-
-}
-printf("Second matrix: \n");
-for(int i=0;i<3;i++){
-for(int j=0;j<3;j++){
-printf(" %d ",b[i][j]);
-}
-printf("\n");
-
-}
-// printing the result
-for (int i=0;i<3;i++)
-{
-  for (int j=0;j<3;j++)
-    sum[i][j]=a[i][j]+b[i][j];
-}
-
-printf("\nSum of two matrices: \n");
-  for (int i = 0; i < 3; i++){
-    for (int j = 0; j < 3; j++) {
-      printf("%d   ", sum[i][j]);
-      }
-      printf("\n");}
-
-//adjoint of matrix
-printf("adjoint of a matrix\n");
-for (i=0;i<3;i++)
-{
-    for(j=0;j<3;j++)
-    {
-       c[i][j]=a[(j+1)%3][(i+1)%3]*a[(j+2)%3][(i+2)%3]-a[(j+1)%3][(i+2)%3]*a[(j+2)%3][(i+1)%3];
-       printf("%d ",c[i][j]);
+    // Input for the first matrix
+    printf("\nEnter elements of 1st matrix:\n");
+    for (i = 0; i < 3; ++i) {
+        for (j = 0; j < 3; ++j) {
+            printf("Enter element a%d%d: ", i + 1, j + 1);
+            scanf("%d", &a[i][j]);
+        }
     }
 
-    printf("\n");
-}
-//determinant
-float determinant;
-
-determinant= a[0][0] * ((a[1][1]*a[2][2]) - (a[2][1]*a[1][2])) -a[0][1] * (a[1][0]
-   * a[2][2] - a[2][0] * a[1][2]) + a[0][2] * (a[1][0] * a[2][1] - a[2][0] * a[1][1]);
-printf("determinant of matrix a");
-printf("\n");
-printf("%f",determinant);
-printf("\n");
-
-
-//inverse of matrix
-printf("inverse of matix c");
-printf("\n");
-for (i=0;i<3;i++)
-{
-    for(j=0;j<3;j++)
-    {
-       I[i][j]=(a[(j+1)%3][(i+1)%3]*a[(j+2)%3][(i+2)%3]-a[(j+1)%3][(i+2)%3]*a[(j+2)%3][(i+1)%3])/determinant;
-       printf("%f ",I[i][j]);
+    // Input for the second matrix
+    printf("\nEnter elements of 2nd matrix:\n");
+    for (i = 0; i < 3; ++i) {
+        for (j = 0; j < 3; ++j) {
+            printf("Enter element b%d%d: ", i + 1, j + 1);
+            scanf("%d", &b[i][j]);
+        }
     }
 
-    printf("\n");
+    // Printing matrices
+    printf("\nFirst matrix: \n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf(" %d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Second matrix: \n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf(" %d ", b[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Sum of two matrices
+    printf("\nSum of two matrices: \n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+            printf("%d   ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Adjoint of matrix
+    printf("Adjoint of a matrix:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            c[i][j] = a[(j + 1) % 3][(i + 1) % 3] * a[(j + 2) % 3][(i + 2) % 3] -
+                      a[(j + 1) % 3][(i + 2) % 3] * a[(j + 2) % 3][(i + 1) % 3];
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Determinant
+    determinant = a[0][0] * ((a[1][1] * a[2][2]) - (a[2][1] * a[1][2])) -
+                  a[0][1] * (a[1][0] * a[2][2] - a[2][0] * a[1][2]) +
+                  a[0][2] * (a[1][0] * a[2][1] - a[2][0] * a[1][1]);
+    printf("Determinant of matrix a: %f\n", determinant);
+
+    // Inverse of matrix
+    printf("Inverse of matrix a:\n");
+    if (determinant == 0) {
+        printf("Inverse does not exist (determinant is zero)\n");
+    } else {
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                I[i][j] = c[i][j] / determinant;
+                printf("%f ", I[i][j]);
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
 }
-
-
-
-  }
